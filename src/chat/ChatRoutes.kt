2bfd1ef38e91de71123f47f26250f2b878320c8f
Odin Kammerloher <kammerloher.andreas@gmail.com>
2021-05-30 13:30:29 +0200

    Add basic echo chat server

---
+++ src/chat/ChatRoutes.kt
@@ -0,0 +1,24 @@
+package de.odinmatthias.routes
+
+import io.ktor.application.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.routing.*
+import io.ktor.websocket.*
+
+
+fun Route.chatRouting() {
+    webSocket("/chat") {
+        send("You are connected!")
+        for (frame in incoming) {
+            frame as? Frame.Text ?: continue
+            val receivedText = frame.readText()
+            send("You said: $receivedText")
+        }
+    }
+}
+
+fun Application.registerChatRouting() {
+    routing {
+        chatRouting()
+    }
+}
